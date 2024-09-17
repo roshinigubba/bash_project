1924164f9f650a084d1c45c418eed0309c67bff4
Roshini Gubba <Roshini.Gubba@ATMECS.CORP>
2024-09-17 21:18:29 +0530

    Updating the script for accessing multiple servers remotely by adding additional comments to clarify its functionality

--- scripts/automate_ssh.sh
+++ scripts/automate_ssh.sh
@@ -14,9 +14,9 @@ SERVER_USERNAME_3="your_username" #Enter your 3rd server username
 KEY_FILE_3="/path/to/key_file_3" #Enter the path to your pem file of 3rd server
 
 
-echo -e "###########connecting to linux_server_1################\n" >>ip_172_31_8_163
+echo -e "###########connecting to linux_server_1################\n" >> file1 #instead of file1 give your server1 ip address as name of file that saves the details of server 1
 
-ssh -i "$KEY_FILE_1" $SERVER_USERNAME_1@$PUBLIC_DNS_1<<EOF >>ip_172_31_8_163
+ssh -i "$KEY_FILE_1" $SERVER_USERNAME_1@$PUBLIC_DNS_1 <<EOF >> file1 #instead of file1 give your server1 ip address as name of file that saves the details of server 1
 
 echo -e "\n"
 
@@ -44,9 +44,9 @@ EOF
 
 echo -e "\n"
 
-echo -e "#########connecting to linux_server_3##################\n" >> ip_54_153_131_173 
+echo -e "#########connecting to linux_server_3##################\n" >>file2 #instead of file2 give your server2 ip address as name of file that saves the details of server 2 
 
-ssh -i "$KEY_FILE_2" $SERVER_USERNAME_2@$PUBLIC_DNS_2<<EOF >>ip_54_153_131_173
+ssh -i "$KEY_FILE_2" $SERVER_USERNAME_2@$PUBLIC_DNS_2 <<EOF >>file2 #instead of file2 give your server2 ip address as name of file that saves the details of server 2
 
 echo -e "\n"
 
@@ -76,9 +76,9 @@ EOF
 
 echo -e "\n"
 
-echo -e "##############connecting to linux_server_4####################\n" >>ip_3_25_77_22
+echo -e "##############connecting to linux_server_4####################\n" >> file3 #instead of file3 give your server3 ip address as name of file that saves the details of server 3
 
-ssh -i "$KEY_FILE_3" $SERVER_USERNAME_3@$PUBLIC_DNS_3<<EOF >>ip_3_25_77_22
+ssh -i "$KEY_FILE_3" $SERVER_USERNAME_3@$PUBLIC_DNS_3 <<EOF >> file3 #instead of file3 give your server3 ip address as name of file that saves the details of server 3
 
 #echo -e "\n"
 
